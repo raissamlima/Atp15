3b49abf66fa3fab7e3d4665a6e8c2d5ede79500c
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-09-29 15:45:02 -0300

    Imprimindo os dados das duas pessoas

--- Dia29/Atv15.java
+++ Dia29/Atv15.java
@@ -11,5 +11,13 @@ class Atv15 {
         p2.nome = "Karolina";
         p2.sobrenome = "Franca";
         p2.idade = 32;
+
+        imprimir(p1);
+        imprimir(p2);
+    }
+    static void imprimir(Pessoa p) {
+        System.out.printf(p.nome);
+        System.out.println(p.sobrenome);
+        System.out.println(p.idade);
     }
 }
