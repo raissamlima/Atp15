175a34d22fa1e45345c15495fd13fac2df030a5f
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-09-30 14:43:29 -0300

    Comentando o código e imprimindo valores

--- Dia29/Atv16.java
+++ Dia29/Atv16.java
@@ -2,6 +2,28 @@ package Dia29;
 
 public class Atv16 {
     public static void main(String[] args) {
-    }
+        CalculadoraTaxas calculadora = new CalculadoraTaxas();
+     
+    // transferência 
+
+        calculadora.transferencia(150.20);
+        calculadora.transferencia(100.50);
     
+    // saque
+
+        calculadora.saque(250.00);
+        calculadora.saque(310.00);
+        calculadora.saque(800.00);
+        calculadora.saque(300.00);
+        calculadora.saque(240.00);
+
+        imprimir(calculadora);
+    }
+
+    // criando método para imprimir 
+
+    static public void imprimir(CalculadoraTaxas calculadora){
+        System.out.printf("O total de taxas de transferência realizado foi: %.2f", calculadora.totalValorCobradoTransferencia);
+        System.out.printf("\nO total de saques realizados foi: %.2f", calculadora.totalValorCobradoSaque);
+    }
 }
--- Dia29/CalculadoraTaxas.java
+++ Dia29/CalculadoraTaxas.java
@@ -1,5 +1,34 @@
 package Dia29;
 
 public class CalculadoraTaxas {
-    
+    public double valorTaxa = 0.001/100;
+    public double valorTaxaSaque = 1.30;
+    private int saquesRealizados = 0;
+    public double totalValorCobradoTransferencia = 0;
+    public double totalValorCobradoSaque = 0;
+
+    // criando o primeiro método de cálculo de taxas nas transferências
+
+    public void transferencia(double valor) {
+        double taxa = multiplicar(valor, valorTaxa);
+        totalValorCobradoTransferencia += taxa;
+    }
+
+    // criando o segundo método de cálculo de taxas em saques
+
+    public void saque(double valor) {
+        double taxa = 0;
+        saquesRealizados += 1;
+        if (saquesRealizados == 5) {
+            saquesRealizados = 0;
+            taxa = valorTaxaSaque;
+        }
+        totalValorCobradoSaque += taxa;
+    }
+
+    // criando método para multiplicar o valor transferido pelo valor da taxa 
+
+    public double multiplicar(double valorTransferido, double valorTaxa) {
+        return valorTransferido * valorTaxa;
+    }
 }
