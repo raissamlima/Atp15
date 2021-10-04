58fc8c3afad79ffc4f030054ac1ee0b4c1706bf0
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-10-04 13:59:48 -0300

    Realizando a chamada dos Métodos e Imprimindo

--- Dia29/TelaPrincipal.java
+++ Dia29/TelaPrincipal.java
@@ -2,6 +2,22 @@ package Dia29;
 
 public class TelaPrincipal {
     public static void main(String[] args) {
-    }
-    
+        ProdutoPrincipal PP = new ProdutoPrincipal();
+        
+        PP.setCodigo(3567);
+        int c = PP.getCodigo();
+        System.out.println(c);
+
+        PP.setNome("Raissa");
+        String n = PP.getNome(); 
+        System.out.println(n);
+
+        PP.setDescricao("Produto 02");
+        String p = PP.getDescricao();
+        System.out.print(p);
+
+        PP.setValor(250.00);
+        double v = PP.getValor();
+        System.out.printf("\nO valor é: %.2f ", v);
+    } 
 }
