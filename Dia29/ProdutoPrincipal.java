5d98470c4df2c5ed81bfc6629961d67d7cf2822b
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-10-04 13:23:03 -0300

    Criando Método Main e a Classe Produto

---
+++ Dia29/ProdutoPrincipal.java
@@ -0,0 +1,8 @@
+package Dia29;
+
+public class ProdutoPrincipal {
+    private int código;
+    private String nome;
+    private String descricao;
+    private double valor;
+}
---
+++ Dia29/TelaPrincipal.java
@@ -0,0 +1,6 @@
+package Dia29;
+
+public class TelaPrincipal {
+    public static void main(String[] args) {
+    } 
+}
