f35f0818f206db7c79d72faa31dbf0d9c564298e
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-10-04 17:40:14 -0300

    Adicionando Valores as posições do Array

--- Dia29/Numeros.java
+++ Dia29/Numeros.java
@@ -3,5 +3,17 @@ package Dia29;
 public class Numeros {
     public static void main(String[] args) {
         int[] n_i = new int[10];
+
+        n_i[0] = 10;
+        n_i[1] = 38;
+        n_i[2] = 56;
+        n_i[3] = 789;
+        n_i[4] = 234;
+        n_i[5] = 645;
+        n_i[6] = 76;
+        n_i[7] = 21;
+        n_i[8] = 89;
+        n_i[9] = 897;
+
     }
 }
