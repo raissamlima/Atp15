f14dcc88f3f8a8bbd3f28babca3ee9769c1fdec1
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-10-04 17:52:42 -0300

    Criando uma classe Pessoa Fisica

---
+++ Dia29/PessoaFisica.java
@@ -0,0 +1,11 @@
+package Dia29;
+
+public class PessoaFisica {
+    private int codigo;
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private String rg;
+    private String cpf;
+
+}
