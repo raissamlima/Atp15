5915abaf0da0e01fe5e5cfb5aaff258179bd9353
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-10-04 17:53:19 -0300

    Criando os Métodos Get e Set

--- Dia29/PessoaFisica.java
+++ Dia29/PessoaFisica.java
@@ -8,4 +8,45 @@ public class PessoaFisica {
     private String rg;
     private String cpf;
 
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
