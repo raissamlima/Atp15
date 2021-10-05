a2ed7800d9a9123e7b8f9a9b98a3ae6288819323
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-10-05 16:02:03 -0300

    Inserindo Valores

--- HerançaDia05/Universidade.java
+++ HerançaDia05/Universidade.java
@@ -4,7 +4,23 @@ public class Universidade {
     public static void main(String[] args) {
 
         Funcionario fc = new Funcionario();
+        fc.setNome("Joaquim");
+        fc.setSobrenome("Lima");
+        fc.setMatricula(3040);
+        fc.setSalario (2.000);
+        fc.setDepartamento("Tecnologia da Informação");
+        
         Coordenador cord = new Coordenador();
+        cord.setSalario(12.000);
+        cord.setDepartamento("Gerencia TI");
+        cord.setCurso_coordenado("Sites Responsivos");
+        cord.setBonus(300.00);
+
         Aluno al = new Aluno();
+        al.setNome("Joaquim");
+        al.setSobrenome("Lima");
+        al.setMatricula(3040);
+        al.setCurso("JavaScript");
+        al.setTurma("Turma A - Noturno");
     }
 }
