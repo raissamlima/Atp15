4f53ce8fd8a447da90c3b3f2030aa5b26799d009
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-10-05 15:44:28 -0300

    Criando Objetos

--- HerançaDia05/Universidade.java
+++ HerançaDia05/Universidade.java
@@ -2,5 +2,9 @@ package HerançaDia05;
 
 public class Universidade {
     public static void main(String[] args) {
+
+        Funcionario fc = new Funcionario();
+        Coordenador cord = new Coordenador();
+        Aluno al = new Aluno();
     }
 }
